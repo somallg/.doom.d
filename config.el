c3664a911fccdc27ef100fb05f2bf5fbd43a9321
somallg <tq.duong@icloud.com>
2020-10-14 10:43:19 +0700

    feat: make Emacs fullscreen on startup

--- config.el
+++ config.el
@@ -53,3 +53,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; maximize/fullscreen Emacs on startup
+(add-to-list 'initial-frame-alist '(fullscreen . fullscreen))
